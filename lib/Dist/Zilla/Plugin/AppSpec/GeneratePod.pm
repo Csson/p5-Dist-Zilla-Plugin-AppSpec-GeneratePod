27e2106aa531f1c39c75659dca19df563eb62053
Erik Carlsson <info@code301.com>
2016-08-25 18:24:41 +0000

    Init

---
+++ lib/Dist/Zilla/Plugin/AppSpec/GeneratePod.pm
@@ -0,0 +1,29 @@
+use 5.10.0;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::AppSpec::GeneratePod;
+
+# ABSTRACT: Short intro
+# AUTHORITY
+our $VERSION = '0.0100';
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::AppSpec::GeneratePod;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::AppSpec::GeneratePod is ...
+
+=head1 SEE ALSO
+
+=cut
